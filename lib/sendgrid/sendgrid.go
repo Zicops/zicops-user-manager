e6b7fc851b5542ee493f55d05402d22c64d96551
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-11-30 15:07:24 -0600

    add

--- lib/sendgrid/sendgrid.go
+++ lib/sendgrid/sendgrid.go
@@ -59,3 +59,20 @@ func (sgc *ClientSendGrid) SendPasswordResetEmail(
 	}
 	return nil
 }
+
+
+// SendInviteToLspEmail ......
+func (sgc *ClientSendGrid) SendInviteToLspEmail(
+	email string,
+	verifyURL string, username string) error {
+	from := mail.NewEmail("Zicops Admin", "noreply@zicops.com")
+	subject := "You have been invited to a learning space"
+	to := mail.NewEmail(username, email)
+	plainTextContent := "Follow the link to view all your learning spaces: " + verifyURL
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, "")
+	_, err := sgc.client.Send(message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
