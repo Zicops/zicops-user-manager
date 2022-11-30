e42690dd7b694af4718aa8ec2087b062674e250e
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2022-11-30 15:46:19 -0600

    join zicops

--- lib/sendgrid/sendgrid.go
+++ lib/sendgrid/sendgrid.go
@@ -32,12 +32,20 @@ func (sgc *ClientSendGrid) InitializeSendGridClient() error {
 func (sgc *ClientSendGrid) SendJoinEmail(
 	email string,
 	verifyURL string, username string) error {
-	from := mail.NewEmail("Zicops Admin", "noreply@zicops.com")
-	subject := "You are invited to join Zicops"
+	mailSetup := mail.NewV3Mail()
+	from := mail.NewEmail("Zicops Admin", "no_reply@zicops.com")
 	to := mail.NewEmail(username, email)
-	plainTextContent := "Follow the link to reset your password: " + verifyURL
-	message := mail.NewSingleEmail(from, subject, to, plainTextContent, "")
-	_, err := sgc.client.Send(message)
+	mailSetup.SetFrom(from)
+	mailSetup.SetTemplateID("d-fec6618de32244f092ee0d45b405501f")
+	p := mail.NewPersonalization()
+	p.AddTos(to)
+	p.SetDynamicTemplateData("b_url", verifyURL)
+	mailSetup.AddPersonalizations(p)
+	request := sendgrid.GetRequest("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", "/v3/mail/send", "https://api.sendgrid.com")
+	request.Method = "POST"
+	var Body = mail.GetRequestBody(mailSetup)
+	request.Body = Body
+	_, err := sendgrid.API(request)
 	if err != nil {
 		return err
 	}
@@ -59,7 +67,6 @@ func (sgc *ClientSendGrid) SendPasswordResetEmail(
 	}
 	return nil
 }
-
 
 // SendInviteToLspEmail ......
 func (sgc *ClientSendGrid) SendInviteToLspEmail(
