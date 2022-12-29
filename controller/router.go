a60cdda363e2c3fea803b8c0763ac248f5f85a6b
Ansh5461 <anshjoshi0607@gmail.com>
2022-12-29 22:36:52 +0530

    Changes

--- controller/router.go
+++ controller/router.go
@@ -66,14 +66,6 @@ func sendOriginInfo(domain string) *model.Organization {
 	}
 	CassUserSession := session
 	queryStr := fmt.Sprintf(`SELECT * from userz.organization where zicops_subdomain='%s' ALLOW FILTERING`, domain)
-
-	/*
-		getQuery := CassUserSession.Query(queryStr, nil)
-		if err := getQuery.SelectRelease(&orgDetails); err != nil {
-			log.Println(err)
-		}
-
-	*/
 	getOrgs := func() (orgDomain userz.Organization, err error) {
 		q := CassUserSession.Query(queryStr, nil)
 		defer q.Release()
