d503e69eefbc05bf6f9db6b8fca9dcf5b3adc9bd
Ansh5461 <anshjoshi0607@gmail.com>
2023-04-04 19:20:33 +0530

    Updated few queries

---
+++ handlers/vendor_order_test.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"testing"
+)
+
+func TestGetAllOrders(t *testing.T) {
+	//run query in db and get result, run from api and get result and compare
+	//either on mutation or queries
+
+	// var ctx context.Context
+	// a := ""
+	// b := ""
+	// c := ""
+	// d := 0
+	// res, err := GetAllOrders(ctx, &a, &b, &c, &d)
+}
